6a836ee36cdd60f459335fb723ea4ee1a30be110
Nadeesha Cabral <n@nadeesha.me>
2024-11-02 09:22:03 +1100

    fix: Refactor demo.ts to streamline URL content fetching and markdown page generation

--- bootstrap-node/src/index.ts
+++ bootstrap-node/src/index.ts
@@ -1,91 +1,41 @@
-import { Inferable } from 'inferable';
-import { z } from 'zod';
+import { Inferable } from "inferable";
+import { z } from "zod";
 
 // Some mock functions to register
-import * as demo from './demo';
+import * as demo from "./demo";
 
 // Instantiate the Inferable client.
 const client = new Inferable({
   // To get a new key, run:
   // npx @inferable/cli auth keys create 'My New Machine Key' --type='cluster_machine'
-  apiSecret: process.env.INFERABLE_API_SECRET
-})
+  apiSecret: process.env.INFERABLE_API_SECRET,
+});
 
 // Register some demo functions
 client.default.register({
-  name: "searchInventory",
-  func: demo.searchInventory,
-  description: "Searches the inventory",
+  name: "getUrlContent",
+  func: demo.getUrlContent,
+  description: "Gets the content of a URL",
   schema: {
     input: z.object({
-      search: z.string().describe("Could match name or description"),
+      url: z.string().describe("The URL to get the content of"),
     }),
   },
 });
 
 client.default.register({
-  name: "getInventoryItem",
-  func: demo.getInventoryItem,
-  description: "Gets an inventory item",
+  name: "generatePage",
+  func: demo.generatePage,
+  description: "Generates a page from markdown",
   schema: {
     input: z.object({
-      id: z.string(),
-    }),
-  },
-});
-
-client.default.register({
-  name: "listOrders",
-  func: demo.listOrders,
-  description: "Lists all orders",
-  schema: {
-    input: z.object({}),
-  },
-});
-
-client.default.register({
-  name: "totalOrderValue",
-  func: demo.totalOrderValue,
-  description: "Calculates the total value of all orders",
-  schema: {
-    input: z.object({}),
-  },
-});
-
-client.default.register({
-  name: "makeOrder",
-  func: demo.makeOrder,
-  description: "Makes an order",
-  config: {
-    requiresApproval: true,
-  },
-  schema: {
-    input: z.object({
-      items: z.array(
-        z.object({
-          id: z.string().describe("Item ID"),
-          qty: z.number().int().positive().describe("Quantity to order"),
-        })
-      ),
+      markdown: z.string().describe("The markdown to generate a page from"),
     }),
   },
 });
 
 client.default.start().then(() => {
   console.log("Inferable demo service started");
-})
+});
 
-// Trigger a Run programmatically
-// https://docs.inferable.ai/pages/runs
-// client.run({
-//   message: "Can you make an order for 2 lightsabers?",
-//   // Optional: Explicitly attach the functions (All functions attached by default)
-//   //attachedFunctions: [],
-//   // Optional: Specify the schema of the result
-//   //resultSchema: z.object({}),
-// }).then(async (run) => {
-//     console.log("Run result", {
-//       result: await run.poll(),
-//     });
-//   });
-
+// To trigger a run: tsx -r dotenv/config src/run.ts
