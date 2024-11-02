2b1dbe7f67d9ab76474ae93d9c7abee4a3c2bf2f
Nadeesha Cabral <n@nadeesha.me>
2024-11-02 15:00:27 +1100

    fix: No working version with inline struct

--- bootstrap-go/main.go
+++ bootstrap-go/main.go
@@ -4,117 +4,65 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/inferablehq/inferable/sdk-go"
+	inferable "github.com/inferablehq/inferable/sdk-go"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-  // Load vars from .env file
-  err := godotenv.Load()
-  if err != nil {
-    panic(err)
-  }
+	// Load vars from .env file
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
 
-  // Instantiate the Inferable client.
-  client, err := inferable.New(inferable.InferableOptions{
-    // To get a new key, run:
-    // npx @inferable/cli auth keys create 'My New Machine Key' --type='cluster_machine'
-    APISecret:     os.Getenv("INFERABLE_API_SECRET"),
-    APIEndpoint:   os.Getenv("INFERABLE_API_ENDPOINT"),
-    ClusterID:     os.Getenv("INFERABLE_CLUSTER_ID"),
-  })
+	// Instantiate the Inferable client.
+	client, err := inferable.New(inferable.InferableOptions{
+		// To get a new key, run:
+		// npx @inferable/cli auth keys create 'My New Machine Key' --type='cluster_machine'
+		APISecret:   os.Getenv("INFERABLE_API_SECRET"),
+		APIEndpoint: os.Getenv("INFERABLE_API_ENDPOINT"),
+		ClusterID:   os.Getenv("INFERABLE_CLUSTER_ID"),
+	})
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  // Register demo functions (Defined in ./src/demo.go)
-  _, err = client.Default.RegisterFunc(inferable.Function{
-    Func:        SearchInventory,
-    Name:        "searchInventory",
-    Description: "Searches the inventory",
-  })
-  if err != nil {
-    panic(err)
-  }
+	// Register functions that match the Node.js implementation
+	_, err = client.Default.RegisterFunc(inferable.Function{
+		Func:        GetUrlContent,
+		Name:        "getUrlContent",
+		Description: "Gets the content of a URL",
+	})
+	if err != nil {
+		panic(err)
+	}
 
-  _, err = client.Default.RegisterFunc(inferable.Function{
-    Func:        GetInventoryItem,
-    Name:        "getInventoryItem",
-    Description: "Gets an inventory item",
-  })
-  if err != nil {
-    panic(err)
-  }
+	_, err = client.Default.RegisterFunc(inferable.Function{
+		Func:        GeneratePage,
+		Name:        "generatePage",
+		Description: "Generates a page from markdown",
+	})
+	if err != nil {
+		panic(err)
+	}
 
-  _, err = client.Default.RegisterFunc(inferable.Function{
-    Func:        ListOrders,
-    Name:        "listOrders",
-    Description: "Lists all orders",
-  })
-  if err != nil {
-    panic(err)
-  }
+	_, err = client.Default.RegisterFunc(inferable.Function{
+		Func:        ScoreHNPost,
+		Name:        "scoreHNPost",
+		Description: "Calculates a score for a Hacker News post given its comment count and upvotes",
+	})
+	if err != nil {
+		panic(err)
+	}
 
-  _, err = client.Default.RegisterFunc(inferable.Function{
-    Func:        TotalOrderValue,
-    Name:        "totalOrderValue",
-    Description: "Calculates the total value of all orders",
-  })
-  if err != nil {
-    panic(err)
-  }
+	err = client.Default.Start()
+	if err != nil {
+		panic(err)
+	}
 
-  type OrderConfig struct {
-    RequiresApproval bool
-  }
+	fmt.Println("Inferable demo service started")
 
-  _, err = client.Default.RegisterFunc(inferable.Function{
-    Func:        MakeOrder,
-    Name:        "makeOrder",
-    Description: "Makes an order",
-    Config:       OrderConfig{RequiresApproval: true},
-  })
-  if err != nil {
-    panic(err)
-  }
-
-  err = client.Default.Start()
-  if err != nil {
-    panic(err)
-  }
-
-  fmt.Println("Inferable Demo is running!")
-
-  defer client.Default.Stop()
-
-  // Trigger a Run programmatically
-  // run, err := client.CreateRun(inferable.CreateRunInput{
-  //   Message: "Can you make an order for 2 lightsabers?",
-  //   // Optional: Explicitly attach the functions (All functions attached by default)
-  //   // AttachedFunctions: []*inferable.FunctionReference{
-  //   //   inferable.FunctionReference{
-  //   //     Function: "SayHello",
-  //   //     Service: "default",
-  //   //   }
-  //   // },
-  //   // Optional: Subscribe an Inferable function to receive notifications when the run status changes
-  //   //OnStatusChange: &inferable.OnStatusChange{
-  //   //  Function: OnStatusChangeFunction
-  //   //}
-  // })
-  //
-  // if err != nil {
-  //   panic(err)
-  // }
-  //
-  // fmt.Println("Run started: ", run.ID)
-  // result, err := run.Poll(nil)
-  // if err != nil {
-  //   panic(err)
-  // }
-  // fmt.Println("Run Result: ", result)
-
-  // Wait for CTRL+C
-  <-make(chan struct{})
+	// Wait for CTRL+C
+	<-make(chan struct{})
 }
---
+++ bootstrap-go/run.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+
+	inferable "github.com/inferablehq/inferable/sdk-go"
+)
+
+type Post struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Points      string `json:"points"`
+	CommentsURL string `json:"commentsUrl"`
+}
+
+type ExtractResult struct {
+	Posts []Post `json:"posts"`
+}
+
+type KeyPoint struct {
+	ID        string   `json:"id"`
+	Title     string   `json:"title"`
+	KeyPoints []string `json:"keyPoints"`
+}
+
+type GeneratePageResult struct {
+	PagePath string `json:"pagePath"`
+}
+
+func RunHNExtraction() error {
+	client, err := inferable.New(inferable.InferableOptions{
+		APISecret:   os.Getenv("INFERABLE_API_SECRET"),
+		APIEndpoint: os.Getenv("INFERABLE_API_ENDPOINT"),
+		ClusterID:   os.Getenv("INFERABLE_CLUSTER_ID"),
+	})
+	if err != nil {
+		return err
+	}
+
+	// Extract top posts
+	extractRun, err := client.CreateRun(inferable.CreateRunInput{
+		Message: `
+		Hacker News has a homepage at https://news.ycombinator.com/
+		Each post has a id, title, a link, and a score, and is voted on by users.
+		Score the top 10 posts and pick the top 3 according to the internal scoring function.
+		`,
+	})
+	if err != nil {
+		return err
+	}
+
+	extractResult, err := extractRun.Poll(nil)
+	if err != nil {
+		return err
+	}
+
+	var posts ExtractResult
+	resultBytes, ok := extractResult.Result.([]byte)
+	if !ok {
+		return fmt.Errorf("failed to convert extract result to []byte")
+	}
+	if err := json.Unmarshal(resultBytes, &posts); err != nil {
+		return err
+	}
+
+	// Summarize each post
+	var summaries []KeyPoint
+	for _, post := range posts.Posts {
+		summarizeRun, err := client.CreateRun(inferable.CreateRunInput{
+			Message: fmt.Sprintf(`
+			<data>
+				%s
+			</data>
+
+			You are given a post from Hacker News, and a url for the post's comments.
+			Summarize the comments. You should visit the comments URL to get the comments.
+			Produce a list of the key points from the comments.
+			`, post),
+		})
+		if err != nil {
+			return err
+		}
+
+		summarizeResult, err := summarizeRun.Poll(nil)
+		if err != nil {
+			return err
+		}
+
+		var summary KeyPoint
+		resultBytes, ok := summarizeResult.Result.([]byte)
+		if !ok {
+			return fmt.Errorf("failed to convert summarize result to []byte")
+		}
+		if err := json.Unmarshal(resultBytes, &summary); err != nil {
+			return err
+		}
+		summaries = append(summaries, summary)
+	}
+
+	// Generate final page
+	generateRun, err := client.CreateRun(inferable.CreateRunInput{
+		Message: fmt.Sprintf(`
+		<data>
+			%s
+		</data>
+
+		You are given a list of posts from Hacker News, and a summary of the comments for each post.
+
+		Generate a web page with the following structure:
+		- A header with the title of the page
+		- A list of posts, with the title, a link to the post, and the key points from the comments in a ul
+		- A footer with a link to the original Hacker News page
+		`, summaries),
+	})
+	if err != nil {
+		return err
+	}
+
+	generateResult, err := generateRun.Poll(nil)
+	if err != nil {
+		return err
+	}
+
+	var pageResult GeneratePageResult
+	resultBytes, ok = generateResult.Result.([]byte)
+	if !ok {
+		return fmt.Errorf("failed to convert generate result to []byte")
+	}
+	if err := json.Unmarshal(resultBytes, &pageResult); err != nil {
+		return err
+	}
+
+	fmt.Printf("Generated page: %+v\n", pageResult)
+
+	// Open browser
+	cmd := exec.Command("open", fmt.Sprintf("https://app.inferable.ai/clusters/%s/runs", os.Getenv("INFERABLE_CLUSTER_ID")))
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Failed to open browser: %v\n", err)
+	}
+
+	return nil
+}
