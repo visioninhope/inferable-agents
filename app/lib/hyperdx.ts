f0c432e57970c35cd3e98fbc74009ad1013e9ced
John Smith <john@johnjcsmith.com>
2025-01-09 15:09:38 +1030

    chore: Dissable hyperDX replays in FE (#525)

--- app/lib/hyperdx.ts
+++ app/lib/hyperdx.ts
@@ -3,5 +3,6 @@ import HyperDX from '@hyperdx/browser';
 process.env.NEXT_PUBLIC_HYPERDX_API_KEY && HyperDX.init({
   apiKey: process.env.NEXT_PUBLIC_HYPERDX_API_KEY,
   service: 'app',
-  tracePropagationTargets: [process.env.NEXT_PUBLIC_INFERABLE_API_URL ?? ''],
+  tracePropagationTargets: [process.env.NEXT_PUBLIC_INFERABLE_API_URL ?? '', process.env.NEXT_PUBLIC_APP_URL ?? ''],
+  disableReplay: true
 });
