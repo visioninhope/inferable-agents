5dcc0d1c584d3163c4f9c4ae3693ed92bf203fe0
John Smith <john@johnjcsmith.com>
2025-01-09 14:58:10 +1030

    chore: Disable hyperdx advanced capture (#524)

--- app/lib/hyperdx.ts
+++ app/lib/hyperdx.ts
@@ -4,6 +4,4 @@ process.env.NEXT_PUBLIC_HYPERDX_API_KEY && HyperDX.init({
   apiKey: process.env.NEXT_PUBLIC_HYPERDX_API_KEY,
   service: 'app',
   tracePropagationTargets: [process.env.NEXT_PUBLIC_INFERABLE_API_URL ?? ''],
-  consoleCapture: true,
-  advancedNetworkCapture: true,
 });
